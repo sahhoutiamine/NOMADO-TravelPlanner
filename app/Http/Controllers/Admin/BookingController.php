35045720c9ba7e1a9b69febd5a01895e392f178e
𝚂𝙰𝙷𝙷𝙾𝚄𝚃𝙸 𝙰𝙼𝙸𝙽𝙴 <midoriaduko@gmail.com>
2026-03-23 03:26:38 +0100

    feat(admin): add admin BookingController to list all reservations

---
+++ app/Http/Controllers/Admin/BookingController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Booking;
+
+class BookingController extends Controller
+{
+    public function index()
+    {
+        $bookings = Booking::with(['user', 'country', 'hotel'])->latest()->get();
+        return view('admin.bookings.index', compact('bookings'));
+    }
+}
