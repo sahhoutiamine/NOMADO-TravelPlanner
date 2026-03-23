010c59ae632bcb05bf290fe20249a44618525c37
𝚂𝙰𝙷𝙷𝙾𝚄𝚃𝙸 𝙰𝙼𝙸𝙽𝙴 <midoriaduko@gmail.com>
2026-03-23 03:24:26 +0100

    feat(models): add role field and bookings relationship to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,12 +16,17 @@ class User extends Authenticatable
      * The attributes that are mass assignable.
      *
      * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'role',
     ];
+
+    public function bookings()
+    {
+        return $this->hasMany(Booking::class);
+    }
 
     /**
      * The attributes that should be hidden for serialization.
