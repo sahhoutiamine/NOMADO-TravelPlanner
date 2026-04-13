4cd0950a5927443122cb1dfed79f6e2d0af2da08
𝚂𝙰𝙷𝙷𝙾𝚄𝚃𝙸 𝙰𝙼𝙸𝙽𝙴 <midoriaduko@gmail.com>
2026-04-13 13:29:54 +0200

    refactor(models): replace hotels/places relations with cities relation in Country

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -4,32 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Country extends Model
 {
-    /** @use HasFactory<\Database\Factories\CountryFactory> */
     use HasFactory;
 
-    protected $fillable = [
-        'name',
-        'description',
-        'image',
-    ];
+    protected $fillable = ['name', 'description', 'image'];
 
-    /**
-     * Get the hotels for the country.
-     */
-    public function hotels(): HasMany
+    public function cities()
     {
-        return $this->hasMany(Hotel::class);
-    }
-
-    /**
-     * Get the places to visit for the country.
-     */
-    public function places(): HasMany
-    {
-        return $this->hasMany(Place::class);
+        return $this->hasMany(City::class);
     }
 }
