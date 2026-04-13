676af764b3eac96c94bc4e716fcd601c5e745b51
𝚂𝙰𝙷𝙷𝙾𝚄𝚃𝙸 𝙰𝙼𝙸𝙽𝙴 <midoriaduko@gmail.com>
2026-04-13 13:30:17 +0200

    refactor(migrations): remove trip_type column from countries table

--- database/migrations/2026_03_23_013934_create_countries_table.php
+++ database/migrations/2026_03_23_013934_create_countries_table.php
@@ -6,24 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('countries', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->enum('trip_type', ['adventure', 'culture', 'beach', 'romantic', 'nature', 'shopping'])->index();
             $table->text('description')->nullable();
             $table->string('image')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('countries');
