a47beaebf88366f9102a1a6e14ead92dcdf4769e
𝚂𝙰𝙷𝙷𝙾𝚄𝚃𝙸 𝙰𝙼𝙸𝙽𝙴 <midoriaduko@gmail.com>
2026-04-13 12:40:18 +0200

    feat(places): add PlaceController with index and show actions

---
+++ app/Http/Controllers/PlaceController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Place;
+use App\Models\Country;
+use Illuminate\Http\Request;
+
+class PlaceController extends Controller
+{
+    public function index(Request $request)
+    {
+        $query = Place::with(['country', 'hotels']);
+
+        if ($request->has('country_id') && $request->country_id != '') {
+            $query->where('country_id', $request->country_id);
+        }
+
+        if ($request->has('search') && $request->search != '') {
+            $query->where('name', 'like', '%' . $request->search . '%');
+        }
+
+        $places = $query->paginate(12);
+        $countries = Country::all();
+
+        return view('places.index', compact('places', 'countries'));
+    }
+
+    public function show($id)
+    {
+        $place = Place::with(['country', 'hotels'])->findOrFail($id);
+        return view('places.show', compact('place'));
+    }
+}
