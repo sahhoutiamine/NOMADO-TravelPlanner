603f011897ab0b5d199bb2cd90afccf33f3d745c
𝚂𝙰𝙷𝙷𝙾𝚄𝚃𝙸 𝙰𝙼𝙸𝙽𝙴 <midoriaduko@gmail.com>
2026-04-14 15:54:55 +0200

    feat(models): add localisation field and country/hotels relationship methods to Place

--- app/Models/Place.php
+++ app/Models/Place.php
@@ -9,10 +9,22 @@ class Place extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['city_id', 'name', 'description', 'image'];
+    protected $fillable = ['city_id', 'name', 'description', 'image', 'localisation'];
 
     public function city()
     {
         return $this->belongsTo(City::class);
+    }
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class, 'city_id', 'id', 'cities', 'country_id');
+    }
+
+    public function hotels()
+    {
+        // This assumes a place can have related hotels if we want to show them
+        // For now, let's keep it consistent with what the controller or seeder expects
+        return $this->city->hotels();
     }
 }
