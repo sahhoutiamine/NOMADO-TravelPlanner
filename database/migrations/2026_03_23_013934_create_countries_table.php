03b6e6f062dce119c229d72911aafa0f62702f3b
𝚂𝙰𝙷𝙷𝙾𝚄𝚃𝙸 𝙰𝙼𝙸𝙽𝙴 <midoriaduko@gmail.com>
2026-04-22 13:30:27 +0200

    feat(migration): add cardinals column to countries table

--- database/migrations/2026_03_23_013934_create_countries_table.php
+++ database/migrations/2026_03_23_013934_create_countries_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
             $table->string('name')->unique();
             $table->text('description')->nullable();
             $table->string('image')->nullable();
+            $table->string('cardinals')->nullable();
             $table->timestamps();
         });
     }
