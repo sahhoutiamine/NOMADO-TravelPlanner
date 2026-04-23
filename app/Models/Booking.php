1ffa972f67e1491746694ba411d072b073158387
𝚂𝙰𝙷𝙷𝙾𝚄𝚃𝙸 𝙰𝙼𝙸𝙽𝙴 <midoriaduko@gmail.com>
2026-04-23 13:54:58 +0200

    feat(model): add flight_airline, flight_class, flight_price, flight_duration to Booking fillable

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -14,6 +14,7 @@ class Booking extends Model
         'budget_total', 'duration', 'passengers',
         'flight_budget', 'hotel_budget', 'activities_budget', 'misc_budget',
         'total_price', 'status', 'selected_place_ids', 'include_hotel',
+        'flight_airline', 'flight_class', 'flight_price', 'flight_duration',
     ];
 
     public function user()
