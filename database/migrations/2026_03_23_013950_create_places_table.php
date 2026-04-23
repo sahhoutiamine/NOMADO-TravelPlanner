dacd6d9c3481042832370b0d7c65470cb6f88fdd
𝚂𝙰𝙷𝙷𝙾𝚄𝚃𝙸 𝙰𝙼𝙸𝙽𝙴 <midoriaduko@gmail.com>
2026-04-23 10:29:15 +0200

    feat(migrations): add min_price column to places table

--- database/migrations/2026_03_23_013950_create_places_table.php
+++ database/migrations/2026_03_23_013950_create_places_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('places', function (Blueprint $table) {
@@ -14,6 +13,7 @@ return new class extends Migration
             $table->string('name');
             $table->text('description')->nullable();
             $table->string('localisation')->nullable(); // Format: "latitude, longitude"
+            $table->decimal('min_price', 10, 2)->default(150.00);
             $table->string('image')->nullable();
             $table->timestamps();
         });
